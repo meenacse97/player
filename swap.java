8bfe71adf5c2866207b2c44182da75ff35dbc9a8
meenacseengineering5 <meenacse1997@gmail.com>
2017-07-21 16:04:28 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,23 @@
+package guvi;
+import java.util.*;
+public class swap {
+	public static void main(String[] args) {
+		System.out.print("string: ");
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		StringBuffer a=new StringBuffer();
+		char[] ch=str.toCharArray();
+		int i;
+		for(i=0;i<ch.length;i++){
+			if(i%2==0){
+				if((i+1)<ch.length){
+				a.append(ch[i+1]);
+			}
+				a.append(ch[i]);
+				}
+		}
+			System.out.print(a);
+	}
+
+
+}
