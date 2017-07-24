c237564e6f4492e964409e0f9d1a59af8056aabb
meenacseengineering5 <meenacse1997@gmail.com>
2017-07-24 14:28:02 +0530

    Add files via upload

---
+++ strlen.java
@@ -0,0 +1,14 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class strlen {
+	  public static void main(String args[]){
+		   String a;
+		   Scanner s=new Scanner(System.in);
+		   a=s.next();
+		   System.out.println("The length of the string is"+a.length());
+		   }
+		   }
+
+
